544b9dd5c9609120714a9161097ebbd4978a212b
MD. FARHAN LABIB JAHIN <jahin2000.11.15@hmail.com>
2023-09-05 03:41:53 +0600

    v1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class main_product(models.Model):
+    p_id = models.AutoField(primary_key=True)
+    p_name = models.CharField(max_length=255)
+    p_type = models.CharField(max_length=255)
+    p_generics = models.CharField(max_length=255)
+    p_company = models.CharField(max_length=255)
+    p_price = models.DecimalField(max_digits=10, decimal_places=2)
+    p_discount = models.DecimalField(max_digits=5, decimal_places=2)
+    p_Indications = models.TextField()
+    p_Pharmacology = models.TextField()
+    p_Dosage = models.TextField()
+    p_Interaction = models.TextField()
+    p_Contradictions = models.TextField()
+    p_Side_Effects = models.TextField()
+    p_Pregnancy = models.TextField()
+    p_Precautions = models.TextField()
+    p_Therapeutic = models.TextField()
+    p_Storage = models.CharField(max_length=255)
+
+    
--- products/views.py
+++ products/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+from .models import main_product
 
 # Create your views here.
-def prod(request,p_name):
+def prod(request, p_name):
     product_details = {
         'name': p_name
-    
-        # Add other product details here
     }
-    
-    return render(request,'product.html',{'product_details': product_details})
+
+    try:
+        product = main_product.objects.get(p_name=product_details['name'])
+    except main_product.DoesNotExist:
+        product = None
+
+    return render(request, 'product.html', {'product_details': product})
+
