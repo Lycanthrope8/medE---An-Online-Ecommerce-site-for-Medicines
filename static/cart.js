9f98fa7ea64eca6f0b0335ef92fb56e73e09871c
lycanthrope8 <jubayerhossain9999@gmail.com>
2023-09-21 03:33:11 +0600

    Cart v2

--- static/cart.js
+++ static/cart.js
@@ -22,53 +22,52 @@ function ClearCart() {
   }
 
 
-// When clicking the cart button
-document.getElementById("cart-btn").addEventListener("click", function() {
-  var cart = JSON.parse(localStorage.getItem('cart'));
 
-  for (var p_id in cart) {
+  document.getElementById("cart-btn").addEventListener("click", function() {
+    var cart = JSON.parse(localStorage.getItem('cart'));
+    var resultsDiv = $('#cart-container');
+    
+    // Clear the existing content
+    resultsDiv.empty();
+  
+    for (var p_id in cart) {
       p_id = parseInt(p_id);
-      console.log("Product ID:", p_id);
-      console.log("Quantity:", cart[p_id]);
-
+  
       // Fetch the product information for the current p_id
       fetch(`get_product_info/${p_id}/`)
-          .then(response => {
-              if (response.ok) {
-                  return response.json();
-              }
-              throw new Error('Network response was not ok.');
-          })
-          .then(productData => {
-              console.log("Product Data:", productData);
-
-              // Find the HTML elements to update based on the product ID
-              var productNameElement = document.getElementById(`productName`);    //productName-${p_id}
-              var productPriceElement = document.getElementById(`productPrice`);  //productPrice-${p_id}
-
-              var productPrice = productData.p_price - (productData.p_price * (productData.p_discount / 100))
-              console.log(productData.p_name);
-              console.log(productPrice);
-
-              
-              // Update the HTML elements with the product information
-              if (productNameElement && productPriceElement) {
-                
-                productNameElement.innerHTML = productData.p_name
-                productPriceElement.innerHTML = productPrice
-                  // Add similar code for other elements as needed
-              }
-          })
-          .catch(error => {
-              console.error('Error:', error);
-          });
-  }
-});
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        })
+        .then(productData => {
+          console.log("Product Data:", productData);
+          var productPrice = productData.p_price - (productData.p_price * (productData.p_discount / 100));
+          // 
+          // Check if productData is not empty
+          if (Object.keys(productData).length > 0) {
+            if ("p_name" in productData) {
+              // Display the product name
+              resultsDiv.append('<li style="display: inline; white-space: nowrap;">Name: ' + productData["p_name"] + '  Quantity: <button class="quantity-button" style="display: inline; white-space: nowrap;">-</button> ' + cart[productData["p_id"]] + ' <button class="quantity-button" style="display: inline; white-space: nowrap;">+</button></li>');
+           } else {
+              resultsDiv.append('<p>No product name found.</p>');
+            }
+          } else {
+            resultsDiv.append('<p>No results found.</p>');
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  });
+  
 
 
 
 
-
+    
 
 
 
