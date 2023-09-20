0542d29266e7ff08ca47a9826ff48e7d6398b8c5
lycanthrope8 <jubayerhossain9999@gmail.com>
2023-09-20 13:14:32 +0600

    v1

--- Home/models.py
+++ Home/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
     p_price = models.DecimalField(max_digits=10, decimal_places=2)
     p_discount = models.DecimalField(max_digits=5, decimal_places=2)
     p_id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+      return self.p_name
