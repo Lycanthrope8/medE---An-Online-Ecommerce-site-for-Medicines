04f0fd1c24c7e919ea7d070f63174e5f637f62ad
MD. FARHAN LABIB JAHIN <jahin2000.11.15@hmail.com>
2023-09-20 21:05:37 +0600

    v2

--- static/search.js
+++ static/search.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     // Function to fetch and display search results
     function fetchSearchResults(query) {
+        // Check if the query is empty
+        if (!query) {
+            $('#search-results').empty();
+            return;
+        }
+
         // Make an AJAX request to your Django live_search view
         $.ajax({
             url: '/live_search/', // Update this URL to match your live_search view URL
@@ -12,8 +18,11 @@ $(document).ready(function () {
 
                 if (data.length > 0) {
                     $.each(data, function (index, item) {
-                        // Display the search results
-                        resultsDiv.append('<a>' + item.p_name + '</a>');
+                        // Construct the URL for the product details page
+                        var url = '/product/' + item.p_name + '/';
+
+                        // Display the search results with the constructed URL
+                        resultsDiv.append('<li><a href="' + url + '">' + item.p_name + '</a></li>');
                     });
                 } else {
                     resultsDiv.append('<p>No results found.</p>');
@@ -36,3 +45,54 @@ $(document).ready(function () {
     });
 });
 
+
+
+
+
+
+$(document).ready(function () {
+    // Function to fetch and display search results
+    function fetchSearchResults(query) {
+        // Check if the query is empty
+        if (!query) {
+            $('#search-results2').empty();
+            return;
+        }
+
+        // Make an AJAX request to your Django live_search view
+        $.ajax({
+            url: '/live_search/', // Update this URL to match your live_search view URL
+            data: { 'q': query },
+            dataType: 'json',
+            success: function (data) {
+                var resultsDiv = $('#search-results2');
+                resultsDiv.empty(); // Clear previous search results
+
+                if (data.length > 0) {
+                    $.each(data, function (index, item) {
+                        // Construct the URL for the product details page
+                        var url = '/product/' + item.p_name + '/';
+
+                        // Display the search results with the constructed URL
+                        resultsDiv.append('<li><a href="' + url + '">' + item.p_name + '</a></li>');
+                    });
+                } else {
+                    resultsDiv.append('<p>No results found.</p>');
+                }
+            }
+        });
+    }
+
+    $('#search-input2').on('input', function () {
+        var query = $(this).val();
+        fetchSearchResults(query);
+    });
+
+    // Clear search results when clicking somewhere else on the page
+    $(document).on('click', function (event) {
+        var target = $(event.target);
+        if (!target.is('#search-input2') && !target.is('#search-results2')) {
+            $('#search-results2').empty();
+        }
+    });
+});
