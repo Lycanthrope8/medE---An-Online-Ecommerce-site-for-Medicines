9fd2c0e3fb0815fed96df2e6eb055be9f4348de1
MD. FARHAN LABIB JAHIN <jahin2000.11.15@hmail.com>
2023-09-20 15:53:46 +0600

    v1

--- products/models.py
+++ products/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from Home.models import Product
+
 class main_product(models.Model):
     CATEGORY_CHOICES = (
         ('BABY CARE', 'BABY CARE'),
         ('MENS','MENS')
-      )
-    feature_CHOICES=(('yes','yes'),
-                      ('no','no'))
-        
+    )
+    feature_CHOICES = (('yes', 'yes'), ('no', 'no'))
+
     p_id = models.AutoField(primary_key=True)
     p_name = models.CharField(max_length=255)
     p_type = models.CharField(max_length=255)
@@ -25,32 +27,34 @@ class main_product(models.Model):
     p_Precautions = models.TextField()
     p_Therapeutic = models.TextField()
     p_Storage = models.CharField(max_length=255)
-    p_category=models.CharField(max_length=255, choices=CATEGORY_CHOICES)
-    feature=models.CharField(max_length=255, choices=feature_CHOICES)
+    p_category = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
+    feature = models.CharField(max_length=255, choices=feature_CHOICES)
 
     def __str__(self):
-      return self.p_name
-    
+        return self.p_name
+
     def save(self, *args, **kwargs):
         super(main_product, self).save(*args, **kwargs)
-        
+
         # Check if the feature is 'yes' and create a corresponding Product entry
         if self.feature == 'yes':
             Product.objects.create(
                 p_name=self.p_name,
                 p_category=self.p_category,
-                p_price = self.p_price,
-                p_discount = self.p_discount,
-                p_id =self.p_id
+                p_price=self.p_price,
+                p_discount=self.p_discount,
+                p_id=self.p_id
             )
-    def delete(self, *args, **kwargs):
-        # Delete the associated Product when deleting main_product
-        if self.feature == 'yes':
-            try:
-                product_to_delete = Product.objects.get(p_id=self.p_id)
-                product_to_delete.delete()
-            except Product.DoesNotExist:
-                pass  # Handle the case where the Product does not exist
 
-        super(main_product, self).delete(*args, **kwargs)
-    
+@receiver(pre_save, sender=main_product)
+def delete_product_if_feature_changed(sender, instance, **kwargs):
+    # Check if the 'feature' field is transitioning from 'yes' to 'no'
+    if instance.pk and instance.feature == 'no':
+        try:
+            product_to_delete = Product.objects.get(p_id=instance.p_id)
+            product_to_delete.delete()
+        except Product.DoesNotExist:
+            pass  # Handle the case where the Product does not exist
+
+# Register the signal
+pre_save.connect(delete_product_if_feature_changed, sender=main_product)
