8ad14db1eb04ec361e97ac8875dc88af62b4c291
MD. FARHAN LABIB JAHIN <jahin2000.11.15@hmail.com>
2023-09-20 14:59:48 +0600

    v1

--- products/models.py
+++ products/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from Home.models import Product
 class main_product(models.Model):
     CATEGORY_CHOICES = (
         ('BABY CARE', 'BABY CARE'),
         ('MENS','MENS')
       )
+    feature_CHOICES=(('yes','yes'),
+                      ('no','no'))
         
     p_id = models.AutoField(primary_key=True)
     p_name = models.CharField(max_length=255)
@@ -24,8 +26,22 @@ class main_product(models.Model):
     p_Therapeutic = models.TextField()
     p_Storage = models.CharField(max_length=255)
     p_category=models.CharField(max_length=255, choices=CATEGORY_CHOICES)
+    feature=models.CharField(max_length=255, choices=feature_CHOICES)
 
     def __str__(self):
       return self.p_name
+    
+    def save(self, *args, **kwargs):
+        super(main_product, self).save(*args, **kwargs)
+        
+        # Check if the feature is 'yes' and create a corresponding Product entry
+        if self.feature == 'yes':
+            Product.objects.create(
+                p_name=self.p_name,
+                p_category=self.p_category,
+                p_price = self.p_price,
+                p_discount = self.p_discount,
+                p_id =self.p_id
+            )
 
     
