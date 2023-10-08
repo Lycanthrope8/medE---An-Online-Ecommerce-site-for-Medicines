fa3a1e88b53d64b6645ed86b3f7e0737d40958d9
lycanthrope8 <jubayerhossain9999@gmail.com>
2023-10-08 20:22:10 +0600

    Admin Panel Fix

--- Home/admin.py
+++ Home/admin.py
@@ -4,12 +4,5 @@ from django.contrib import admin
 from .models import Product
 from authentication.models import UserProfile
 
-class UserProfileAdmin(admin.ModelAdmin):
-    # Customize the display in the admin panel if needed
-    list_display = ('phone_number',)
-
-# Register the UserProfile model with the custom admin configuration
-admin.site.register(UserProfile, UserProfileAdmin)
-
+admin.site.register(UserProfile)
 admin.site.register(Product)
-# admin.site.register(UserProfile)
