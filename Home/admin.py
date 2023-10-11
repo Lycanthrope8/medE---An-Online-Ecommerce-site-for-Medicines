ec32c333c260f3f9821066631fea209bb6f26d91
lycanthrope8 <jubayerhossain9999@gmail.com>
2023-10-12 03:05:01 +0600

    Database fixed. All attributes added

--- Home/admin.py
+++ Home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product
-from authentication.models import UserProfile
+from authentication.models import UserProfile 
 
 admin.site.register(UserProfile)
 admin.site.register(Product)
