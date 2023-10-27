04fb88cc9c99f605bbb360246bdb681d7e38ae40
Mehedi Hasan Nabil <37480357+NOCKTOWL@users.noreply.github.com>
2023-10-27 23:47:42 +0600

    design fixes

--- medE/settings.py
+++ medE/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'livereload.middleware.LiveReloadScript',
+    'livereload.middleware.LiveReloadScript',
     
 ]
 
