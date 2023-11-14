627914e7cfb0c84085df6e757bbeaab80e8d026c
MD. FARHAN LABIB JAHIN <jahin2000.11.15@hmail.com>
2023-11-14 18:04:21 +0600

    ordering process done

--- products/admin.py
+++ products/admin.py
@@ -11,11 +11,22 @@ admin.site.register(main_product)
 class OrdersAdmin(admin.ModelAdmin):
     list_display = ('phonenumber', 'status', 'timestamp')  # Add any other fields you want to display
     list_filter = ('status',)  # Filter by the 'status' field
+    search_fields = ('phonenumber',)
+    def photos_display(self, obj):
+        if obj.prescriptions:
+            return format_html('<img src="{}{}" height="500" />', settings.MEDIA_URL, obj.prescriptions[0])
+        else:
+            return "-"
+
+    photos_display.allow_tags = True
+    photos_display.short_description = 'Prescription Photo'
+    readonly_fields = ('photos_display',)
 
     def get_list_filter(self, request):
         if request.GET.get('status') == 'confirm':
             return ('status',)
         return super().get_list_filter(request)
+    
 
 admin.site.register(Orders, OrdersAdmin)
 
